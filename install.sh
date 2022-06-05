11e1192ceea5e136b60077792f3560e31b4800ca
opx <mail@opx.sh>
2022-06-05 18:38:28 +0200

    Mark script as executable during installation

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ sudo cp "${ORIGIN_DIR}/ideapad-noise-cancelling.conf" /etc/
 sudo cp "${ORIGIN_DIR}/ideapad-noise-cancelling.sh" /opt/
 sudo cp "${ORIGIN_DIR}/ideapad-noise-cancelling.service" /etc/systemd/system/
 
+sudo chmod +x /opt/ideapad-noise-cancelling.sh
+
 sudo systemctl enable ideapad-noise-cancelling.service
 sudo systemctl start ideapad-noise-cancelling.service
